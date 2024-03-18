1d4c38eeab554dd9cbbb4da5c543db783e1091a2
TahsinEfe <thsnfylmz2002@gmail.com>
2024-03-18 17:24:52 +0300

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Intala Lab on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
